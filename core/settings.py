30848f7f47608e04d76e6f889891d6f2a0a2bcbb
dark-hunter0 <afarag70325@yahoo.com>
2026-07-21 15:05:44 +0300

    Add 'Project' model with soft delete functionality and PostgreSQL setup
    - Introduced `Project` model with soft delete support, custom managers, and ordering by creation date.
    - Replaced SQLite with PostgreSQL, configured via environment variables.
    - Initialized migration for the 'projects' app.
    - Updated settings to load environment variables using dotenv.

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from the .env file
+load_dotenv(BASE_DIR / '.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +84,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
